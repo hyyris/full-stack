bd0168e59864018a69d462ed7ab0b9e06f216db7
Eetu Hyyryläinen <eetu.hyyrylainen@siilim190901.local>
2022-05-20 13:14:42 +0300

    Refactor part1 q

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,19 +1,27 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const exercises = {
-    part1: 'Fundamentals of React',
-    exercises1: 10,
-    part2: 'Using props to pass data',
-    exercises2: 7,
-    part3: 'State of a component',
-    exercises3: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
   
   return (
     <div>
-      <Header course={course} />
-      <Content exercises={exercises} />
-      <Total total={exercises.exercises1 + exercises.exercises2 + exercises.exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -27,9 +35,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.exercises.part1} count={props.exercises.exercises1} />
-      <Part name={props.exercises.part2} count={props.exercises.exercises2} />
-      <Part name={props.exercises.part3} count={props.exercises.exercises3} />
+      <Part name={props.parts[0].name} count={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} count={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} count={props.parts[2].exercises} />
     </div>
   )
 }
@@ -44,7 +52,7 @@ const Part = (props) => {
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 

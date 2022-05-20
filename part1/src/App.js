decb585d676ce178b45591047af238caee66dd27
Eetu Hyyryläinen <eetu.hyyrylainen@siilim190901.local>
2022-05-20 11:51:08 +0300

    Add part1.1

---
+++ part1/src/App.js
@@ -0,0 +1,49 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const exercises = {
+    part1: 'Fundamentals of React',
+    exercises1: 10,
+    part2: 'Using props to pass data',
+    exercises2: 7,
+    part3: 'State of a component',
+    exercises3: 14
+  }
+  
+  return (
+    <div>
+      <Header course={course} />
+      <Content exercises={exercises} />
+      <Total total={exercises.exercises1 + exercises.exercises2 + exercises.exercises3} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.exercises.part1} {props.exercises.exercises1}
+      </p>
+      <p>
+        {props.exercises.part2} {props.exercises.exercises2}
+      </p>
+      <p>
+        {props.exercises.part3} {props.exercises.exercises3}
+      </p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+export default App
